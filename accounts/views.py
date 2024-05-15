f347c91aabd20216a49ae853a9038dbfd6c65b53
Severino Murilo <murilo91522608@gmail.com>
2024-05-14 21:27:46 -0300

    GET request and valid form

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def signup(request):
-    return render(request, "accounts/signup.html")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #log the user
+            return redirect('polls:index')
+    form = UserCreationForm()
+    return render(request, "accounts/signup.html", {'form': form})
